127e22cb486a4cbb510bc30042bf860f6c85de7b
urihani <julienrw@gmail.com>
2022-10-21 10:24:47 +0200

    tp3 - mise en place des données pour la génération du menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,7 +1,77 @@
+let menuContent = {
+  accueil: {
+    text: "Accueil",
+    url: "",
+    children: {},
+  },
+  animaux: {
+    text: "Animaux",
+    url: "",
+    children: {},
+  },
+  audio: {
+    text: "Visite virtuelle audio",
+    url: "",
+    children: {},
+  },
+  video: {
+    text: "Visite virtuelle video",
+    url: "",
+    children: {},
+  },
+  contact: {
+    text: "Contact",
+    url: "",
+    children: {},
+  },
+  espacePerso: {
+    text: "Espace perso.",
+    url: "",
+    children: {
+      connexion: {
+        text: "Connexion",
+        url: "",
+        children: {},
+      },
+      informations: {
+        text: "Mes informations",
+        url: "",
+        children: {
+          nom: {
+            text: "Nom & prénom",
+            url: "",
+            children: {},
+          },
+          telephone: {
+            text: "Numéro de téléphone",
+            url: "",
+            children: {},
+          },
+          email: {
+            text: "Email",
+            url: "",
+            children: {},
+          },
+        },
+      },
+      messagerie: {
+        text: "Messagerie",
+        url: "",
+        children: {},
+      },
+      historique: {
+        text: "Historique",
+        url: "",
+        children: {},
+      },
+    },
+  },
+};
+
 /**
  * Permet de dérouler le menu en mode mobile
  */
-function myFunction() {
+function toggleMenu() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
